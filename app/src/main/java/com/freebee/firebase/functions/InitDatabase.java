284c56daf91e48a6f6da1a0a13b3d2598626dc82
WicklowTiger <razvan.miculit@gmail.com>
2022-01-12 12:51:13 +0200

    chore: added InitDatabase local firebase function

---
+++ app/src/main/java/com/freebee/firebase/functions/InitDatabase.java
@@ -0,0 +1,39 @@
+package com.freebee.firebase.functions;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class InitDatabase extends Thread {
+    @Override
+    public void run() {
+        FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+        Map<String, Object> user = new HashMap<>();
+        user.put("first", "Alan");
+        user.put("middle", "Mathison");
+        user.put("last", "Turing");
+        user.put("born", 1912);
+        firestore.collection("users")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d("InitDatabase", "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w("InitDatabase", "Error adding document", e);
+                    }
+                });
+    }
+}
