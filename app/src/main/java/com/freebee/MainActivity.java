c96f87764bda0400d4582dd53be08340293b8164
WicklowTiger <razvan.miculit@gmail.com>
2022-01-12 14:36:48 +0200

    chore, wip: selecting a city now loads the restaurants list for that city

--- app/src/main/java/com/freebee/MainActivity.java
+++ app/src/main/java/com/freebee/MainActivity.java
@@ -14,11 +14,16 @@ import com.google.common.collect.Lists;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private String selectedCountry;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,23 +37,43 @@ public class MainActivity extends AppCompatActivity {
         this.initializeValues();
     }
 
+    /**
+     * Loads initial values from firestore
+     */
     private void initializeValues() {
         FirebaseManager.getCollection("countries", (Callback<QuerySnapshot>) result -> {
             String[] countriesToLoad = Lists.newArrayList(result).stream().map(DocumentSnapshot::getId).toArray(String[]::new);
             Collections.reverse(Arrays.asList(countriesToLoad));
-            loadCountries(countriesToLoad);
+            setCountries(countriesToLoad);
         });
     }
 
+    /**
+     * Load cities for selected country from firestore
+     */
     private void selectCountry(String countryName) {
-        FirebaseManager.getCollection("countries", (Callback<QuerySnapshot>) result -> {
-            String[] countriesToLoad = Lists.newArrayList(result).stream().map(DocumentSnapshot::getId).toArray(String[]::new);
-            Collections.reverse(Arrays.asList(countriesToLoad));
-            loadCountries(countriesToLoad);
+        this.selectedCountry = countryName;
+        FirebaseManager.getCollection("countries/" + countryName + "/cities", (Callback<QuerySnapshot>) result -> {
+            String[] citiesToLoad = Lists.newArrayList(result).stream().map(x -> (String) x.getData().get("name")).toArray(String[]::new);
+            Collections.reverse(Arrays.asList(citiesToLoad));
+            setCities(citiesToLoad);
         });
     }
 
-    private void loadCountries(String[] countries) {
+    /**
+     * Loads restaurants for selected city from firebase
+     */
+    private void selectCity(String cityName) {
+        FirebaseManager.getDocumentByName("countries/" + this.selectedCountry + "/cities", cityName, (Callback<DocumentSnapshot>) result -> {
+            @SuppressWarnings("unchecked")
+            Map<String, ArrayList<Integer>> restaurants = (HashMap<String, ArrayList<Integer>>) result.get("restaurants");
+            if(restaurants != null) {
+                setRestaurants(restaurants);
+            }
+        });
+    }
+
+    private void setCountries(String[] countries) {
         Spinner countrySpinner = findViewById(R.id.countrySpinner);
         ArrayAdapter<String> countryAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, countries);
         countrySpinner.setAdapter(countryAdapter);
@@ -59,27 +84,35 @@ public class MainActivity extends AppCompatActivity {
             }
 
             @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {}
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
         });
     }
 
-    private void selectCity(String cityName) {
-
-    }
-
-    private void loadCities(String[] cities) {
+    private void setCities(String[] cities) {
         Spinner countrySpinner = findViewById(R.id.citySpinner);
         ArrayAdapter<String> countryAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, cities);
         countrySpinner.setAdapter(countryAdapter);
         countrySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                selectCountry(adapterView.getItemAtPosition(i).toString());
+                selectCity(adapterView.getItemAtPosition(i).toString());
             }
 
             @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {}
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
         });
+    }
+
+    private void setRestaurants(Map<String, ArrayList<Integer>> restaurants) {
+        for(String key: restaurants.keySet()) {
+            ArrayList<Integer> prices = restaurants.get(key);
+            if(prices != null && prices.size() == 2) {
+                System.out.println("Panda price: " + prices.get(0));
+                System.out.println("Tazz price: " + prices.get(1));
+            }
+        }
     }
 
 }
