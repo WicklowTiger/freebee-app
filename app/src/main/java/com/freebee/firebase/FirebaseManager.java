160a3a75a7a78fb913ba901174f754659254afee
WicklowTiger <razvan.miculit@gmail.com>
2022-01-12 12:50:40 +0200

    chore: created FirebaseManager wrapper for all firebase operations

---
+++ app/src/main/java/com/freebee/firebase/FirebaseManager.java
@@ -0,0 +1,54 @@
+package com.freebee.firebase;
+
+import android.os.Build;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+
+import com.freebee.firebase.functions.InitDatabase;
+import com.freebee.shared.Callback;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class FirebaseManager {
+    private static final Map<String, Class<? extends Thread>> functions = new HashMap<String, Class<? extends Thread>>() {{
+        put("InitDatabase", InitDatabase.class);
+    }};
+
+    public static void runFunction(String className) {
+        Class<? extends Thread> fnClassDef = functions.get(className);
+        if (fnClassDef == null) {
+            Log.e("fnError", "Function " + className + " does not exist! Add it to FirebaseManager.functions");
+            return;
+        }
+        try {
+            fnClassDef.newInstance().start();
+        } catch (IllegalAccessException | InstantiationException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void getCollection(String path, Callback<QuerySnapshot> fn) {
+        FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+        firestore.collection("countries")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful() && task.getResult() != null) {
+                            fn.call(task.getResult());
+                        } else {
+                            Log.d("FirebaseManager", "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+    }
+}
